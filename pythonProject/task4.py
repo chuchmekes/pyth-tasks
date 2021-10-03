f2e8b8df986404d92651e1ecc5a19c7bbfa3e397
chuchmek <pandapo201988@gmail.com>
2021-10-03 14:07:19 +0300

    initial commit

---
+++ pythonProject/task4.py
@@ -0,0 +1,10 @@
+#Дано число. Если оно меньше 7, то вывести Yes, если больше 10,
+#то вывести No, если равно 9, то вывести Error.
+var=int(input("Введите  число: "))
+if var < 7:
+    print("yes")
+elif var > 10:
+        print("no")
+elif var == 9:
+    print("error")
+
---
+++ pythonProject/task5.py
@@ -0,0 +1,7 @@
+# Дано два числа. Вывести наибольшее из них.
+num1=int(input("Введите первое число: "))
+num2=int(input("Введите второе число: "))
+if num1 >num2:
+    print(num1)
+elif num2 >num1:
+    print(num2)
